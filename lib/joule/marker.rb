1ce18d7ea178494bc98cd73f40bfd292a8e7e2ea
Andrew Olson <anolson@gmail.com>
2009-12-28 13:33:56 -0500

    Making Marker a little more readable.

--- lib/joule/marker.rb
+++ lib/joule/marker.rb
@@ -1,7 +1,25 @@
 class Marker
-  attr_accessor :active, :avg_cadence, :avg_heartrate, :avg_power, :average_power_to_weight, :avg_speed, :comment, 
-  :duration_seconds, :distance, :end, :energy, :intensitry_factor, :max_cadence, :max_heartrate, :max_power, 
-  :maximum_power_to_weight, :max_speed, :normalized_power, :start, :start_time, :training_intensity, :end
+  attr_accessor :active
+  attr_accessor :avg_cadence
+  attr_accessor :avg_heartrate
+  attr_accessor :avg_power
+  attr_accessor :average_power_to_weight
+  attr_accessor :avg_speed
+  attr_accessor :comment
+  attr_accessor :duration_seconds
+  attr_accessor :distance
+  attr_accessor :end
+  attr_accessor :energy
+  attr_accessor :intensitry_factor
+  attr_accessor :max_cadence
+  attr_accessor :max_heartrate
+  attr_accessor :max_power
+  attr_accessor :maximum_power_to_weight
+  attr_accessor :max_speed 
+  attr_accessor :normalized_power 
+  attr_accessor :start
+  attr_accessor :start_time
+  attr_accessor :training_intensity
   
   def initialize(options = {})
     @start = options[:start]
