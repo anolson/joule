2825df0427a7cc5c87475d9d33fdcbbfd8ec2f77
Andrew Olson <anolson@gmail.com>
2010-01-07 16:59:16 -0500

    Fixing TestPowertapParser

--- test/test_powertap_parser.rb
+++ test/test_powertap_parser.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/helper'
 
 module Joule
-  class TestUnitsConverter < Joule::TestCase
+  class TestPowertapParser < Joule::TestCase
     include Joule::UnitsConversion
         
     def test_parse
